bf4b10ad7a50599e70d526889fa32e97698dd17b
rifath33 <hrifath20@stuy.edu>
2020-12-15 16:12:22 -0500

    made classes

--- Sorts.java
+++ Sorts.java
@@ -1,7 +1,16 @@
 public class Sorts{
   /**Bubble sort of an int array.
   *Upon completion, the elements of the array will be in increasing order.
-  *@param data  the elements to be sorted.
   */
-  public static void bubbleSort(int[] data){  }
+
+  //@param data the elements to be sorted.
+  public static void bubbleSort(int[] data){
+    int workingRange = data.length;
+    
+    for(int i = 0; i < data.length; i++){
+
+      for(int j = 0; j < workingRange; j++){
+
+      }
+
 }
