3e8ebd1e9cb0f206118e4c362a5114028b9cf659
Rifath <hrifath20@stuy.edu>
2020-12-17 11:36:10 -0500

    I made bubbleSort. At first I was focused on getting the largest number to the end of the array, and then reducing the working range by 1 each time but after watching the video I learned that we're instead supposed to swap as we go, so for example if data[j] > data[j+i], we should swap them...

--- Sorts.java
+++ Sorts.java
@@ -6,11 +6,19 @@ public class Sorts{
   //@param data the elements to be sorted.
   public static void bubbleSort(int[] data){
     int workingRange = data.length;
-    
+
     for(int i = 0; i < data.length; i++){
 
-      for(int j = 0; j < workingRange; j++){
-
+      for(int j = 0; j < data.length-i-1; j++){
+        int memory = data[j+1];
+        if(data[j] > data[j+1]){
+          data[j+1] = data[j];
+          data[j] = memory;
+        }
       }
+
+    }
+
+  }
 
 }
