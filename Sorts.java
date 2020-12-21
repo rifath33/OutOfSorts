ffcb99dd5d4009572d9fd307add8b5b06268a1cf
Rifath <hrifath20@stuy.edu>
2020-12-21 13:55:51 -0500

    moving work from one computer to another

--- Sorts.java
+++ Sorts.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class Sorts{
   /**Bubble sort of an int array.
   *Upon completion, the elements of the array will be in increasing order.
@@ -5,6 +6,7 @@ public class Sorts{
 
   //@param data the elements to be sorted.
   public static void bubbleSort(int[] data){
+    int stop = 0;
     for(int i = 0; i < data.length; i++){
       for(int j = 0; j < data.length-i-1; j++){
         int memory = data[j+1];
@@ -16,21 +18,48 @@ public class Sorts{
     }
   }
 
-  public static void selectionSort(int[] data){
+/*
+The selection sort is an in-place sorting algorithm. It modifies an array in the
+following way:
 
-    for(int i = 0 ; i < data.length; i++){
-    int memory = data[i];
-    int indexMemory = 0;
-      for(int f = 0; f < data.length-i-1; f++){
-        if(data[f] < memory){
-          memory = data[f];
-          indexMemory = f;
-        }
-      }
-      data[indexMemory] = data[i];
-      data[i] = memory;
+For each index i of the array, starting with the first index:
+a Find the smallest element located at index i or any of the higher indices.
+b Swap that value into index i.
+c Increase i
+d repeat a,b,c until the last index.
+
+*/
+  public static void selectionSort(int[] data){
+    for(int i = 0; i < data.length; i++){
+
+      // for(int f = 0; f < data.length; f++){
+      //   if(data[f] < data[i]){
+      //     swap(data, f, i);
+      //   }
+
+      swap(data, indexOfSmallestElement(data, i, data.length), i);
+
+
     }
-    
+
   }
+
+
+  public static void swap(int[] data, int indexA, int indexB){
+    int memory = data[indexA];
+    data[indexA] = data[indexB];
+    data[indexB] = memory;
+  }
+
+  public static int indexOfSmallestElement(int[] data, int startInclusive,
+  int endNonInclusive){
+    int memory = 0;
+    for(int i = 0; i < endNonInclusive; i++){
+      if(data[i] < data[memory])
+      memory = i;
+    }
+    return memory;
+  }
+
 
 }
--- Tester.java
+++ Tester.java
@@ -1,5 +1,23 @@
+import java.util.Random;
+import java.util.Arrays;
+
 public class Tester{
   public static void main(String[] args){
-    
+
+    int[] sort = {64,25,12,22,11};
+    System.out.println(Arrays.toString(sort));
+    Sorts.selectionSort(sort);
+    System.out.println(Arrays.toString(sort));
+
+    System.out.println(Sorts.indexOfSmallestElement(sort, 0, sort.length));
+    //
+    // //kredit to mr.k for the seed code ;;;;;;)
+    // Random rng = new Random(100);//seed of 100 is stored.
+    //   for(int i =  0; i < sort.length; i++ ){
+    //     sort[i] = (rng.nextInt() % 1000);
+    //   }
+
+    // System.out.println(sort.toString());
+
   }
 }
